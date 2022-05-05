d921680008979c2c128c82cd3f64145a042a7cb1
jhuber <jhuber@htl-steyr.ac.at>
2022-05-05 09:22:27 +0200

    Bitte geh

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian to javafx.fxml;
+    exports com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian;
+}
