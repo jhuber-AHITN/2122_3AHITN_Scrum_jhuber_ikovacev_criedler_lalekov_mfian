7225f161ca9a29fb37b930d61928625000ed64bc
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-05-20 09:13:33 +0200

    Playfield added

--- src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
+++ src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
@@ -38,12 +38,4 @@ public class SchiffeversenkenController {
             }
         }
     }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-            gp.add();
-            }
-        }
-    }
 }
