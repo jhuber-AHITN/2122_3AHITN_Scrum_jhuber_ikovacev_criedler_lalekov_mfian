8a8b073e4931abdba17f447b3766c4978bf3c065
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-05-20 08:29:45 +0200

    Playfield added

--- src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
+++ src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
@@ -1,4 +1,22 @@
 package com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
 public class SchiffeversenkenController {
+
+    @FXML
+    private GridPane gp;
+    @FXML
+    public void onClick(javafx.scene.input.MouseEvent event) {
+        Node clickedNode = event.getPickResult().getIntersectedNode();
+
+        if (clickedNode != gp) {
+            // click on descendant node
+            Integer colIndex = GridPane.getColumnIndex(clickedNode);
+            Integer rowIndex = GridPane.getRowIndex(clickedNode);
+            System.out.println("Mouse clicked cell: " + colIndex + " And: " + rowIndex);
+        }
+    }
 }
