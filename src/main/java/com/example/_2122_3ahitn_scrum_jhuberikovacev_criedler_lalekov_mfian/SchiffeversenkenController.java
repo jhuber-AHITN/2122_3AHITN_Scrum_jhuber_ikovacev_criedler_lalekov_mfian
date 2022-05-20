aff33142dc899e79176413ee00f014dd7a125b7b
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-05-20 09:00:15 +0200

    Playfield added

--- src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
+++ src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
@@ -3,8 +3,19 @@ package com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 
 public class SchiffeversenkenController {
+    int[][] fieldArray ={{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}
+            ,{0,0,0,0,0,0,0,0,0,0}};
 
     @FXML
     private GridPane gp;
@@ -17,6 +28,22 @@ public class SchiffeversenkenController {
             Integer colIndex = GridPane.getColumnIndex(clickedNode);
             Integer rowIndex = GridPane.getRowIndex(clickedNode);
             System.out.println("Mouse clicked cell: " + colIndex + " And: " + rowIndex);
+        }
+    }
+
+    public static void lastShipPlaced(){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+            gp.add();
+            }
         }
     }
 }
