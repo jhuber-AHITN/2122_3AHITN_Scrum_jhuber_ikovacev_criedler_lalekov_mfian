d3b30afb951c789f4877be3aa804faebda6699a8
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-06-09 14:53:13 +0200

    Test

--- src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/model/Spielfeld.java
+++ src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/model/Spielfeld.java
@@ -2,7 +2,7 @@ package com.example._2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian.mod
 
 
 public class Spielfeld {
-    public static final int EMPTY = 0;
+    public static final int EMPTY = 10;
     public static final int SHIP = 1;
     public static final int MISSED = 2;
     public static final int HIT = 3;
