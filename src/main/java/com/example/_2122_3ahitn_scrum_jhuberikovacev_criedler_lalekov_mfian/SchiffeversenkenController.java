d5b0f0cf776efb2e1604bb509c56f3d4e008dab5
jhuber <jhuber@htl-steyr.ac.at>
2022-06-09 14:03:40 +0200

    Comment for Rightclick detection

--- src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
+++ src/main/java/com/example/_2122_3ahitn_scrum_jhuberikovacev_criedler_lalekov_mfian/SchiffeversenkenController.java
@@ -11,9 +11,9 @@ public class SchiffeversenkenController {
     protected Spiel sp;
 
 
-
     @FXML
     private GridPane gp;
+
     @FXML
     public void onClick(javafx.scene.input.MouseEvent event) {
         Node clickedNode = event.getPickResult().getIntersectedNode();
@@ -24,6 +24,34 @@ public class SchiffeversenkenController {
             Integer colIndex = GridPane.getColumnIndex(clickedNode);
             Integer rowIndex = GridPane.getRowIndex(clickedNode);
             System.out.println("Mouse clicked cell: " + colIndex + " And: " + rowIndex);
+
+            /*
+            import javafx.stage.Stage;
+            import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.input.*;
+
+var r = Rectangle {
+    x: 50, y: 50
+    width: 120, height: 120
+    fill: Color.RED
+    onMouseClicked: function(e:MouseEvent):Void {
+        if (e.button == MouseButton.SECONDARY) {
+            println("Right button clicked");
+        }
+    }
+}
+
+Stage {
+    title : "ClickTest"
+    scene: Scene {
+        width: 200
+        height: 200
+        content: [ r ]
+    }
+}
+             */
         }
     }
 
